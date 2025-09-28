328b5b1c2000fcc4ac26ee107adeff2d8e36d59b
Christan <cralleo2000@gmail.com>
2025-09-28 17:59:45 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] inventory = { "hydraulic pump", "PLC module", "servo motor" };
+
+Console.WriteLine("Hej. Welcome to the spare parts inventory!");
+Console.WriteLine("Which part do you need?");
+
+bool found = false;
+
+while (found == false)
+{
+    Console.Write("> ");
+    string input = Console.ReadLine();
+
+    foreach (string part in inventory)
+    {
+        if (input == part)
+        {
+            Console.WriteLine("I have got " + part + " here for you. Bye!");
+            found = true;
+        }
+    }
+
+    if (found == false)
+    {
+        Console.WriteLine("I am afraid we don’t have any " + input + " in the inventory.");
+    }
+}
+ 
